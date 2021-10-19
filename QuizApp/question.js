3cb5b0d6fc59bdd26865a48fda6d3cc2da39a42c
jay669-99 <apawujohn6@gmail.com>
2021-10-19 15:08:39 +0000

    Adding QuizApp

---
+++ QuizApp/question.js
@@ -0,0 +1,142 @@
+var questions = [{
+    "question": "After leaving Egypt, Abram went to what region of Canaan (Gen. 13)?",
+    "option1": "The light and the darkness",
+    "option2": "The heavens and the earth",
+    "option3": "The land and the sea",
+    "option4": "The world and the stars",
+    "answer": "2"
+}, {
+    "question": "God told Abram his descendants would be enslaved for how long (Gen. 15)?", 
+    "option1": "100 years",
+    "option2": "200 years",
+    "option3": "300 years",
+    "option4": "400 years",
+    "answer": "4"
+}, {
+    "question": "When God appeared to Abram in a vision, God likened himself to a (Gen. 15)",
+    "option1": "Rock",
+    "option2": "Fire",
+    "option3": "Shield",
+    "option4": "Sword",
+    "answer": "1"
+}, {
+    "question": "What three animals did God request be cut in two in Abram's vision (Gen. 15)?",
+    "option1": "An ox, heifer and sheep",
+    "option2": "A goat, heifer and ram",
+    "option3": "A goat, sheep and dove",
+    "option4": "A sheep, goat and dove",
+    "answer": "2"
+}, {
+    "question": "Abraham offered the three visitors water to wash (Gen. 18)",
+    "option1": "Their hands",
+    "option2": "Their faces",
+    "option3": "Their feet",
+    "option4": "Their necks",
+    "answer": "3"
+}, {
+    "question": "The cutting of the animals in two and the passing between the parts was a way God made a ________ with Abram (Gen. 15)",
+    "option1": "Deal",
+    "option2": "Prediction",
+    "option3": "Alliance",
+    "option4": "Covenant",
+    "answer": "4"
+}, {
+    "question": "What other river did God give as the boundary of the land promised to Abram besides the Euphrates (Gen. 15)?",
+    "option1": "The Nile",
+    "option2": "The Jordan",
+    "option3": "The river of Egypt",
+    "option4": "The Litani",
+    "answer": "3"
+},{
+    "question": "Hagar was originally from what region (Gen. 16)?",
+    "option1": "Babylon",
+    "option2": "Canaan",
+    "option3": "Egypt",
+    "option4": "Haran",
+    "answer": "3"
+}, {
+    "question": "Who suggested that Abram should have a child with Hagar (Gen. 16)?",
+    "option1": "Hagar",
+    "option2": "Sarai",
+    "option3": "Abram",
+    "option4": "The Lord",
+    "answer": "2"
+}, {
+    "question": "What was Hagar's response when she found out she was with child (Gen. 16)?",
+    "option1": "She despised Sarai",
+    "option2": "She feared Sarai",
+    "option3": "She loved Abram",
+    "option4": "She loved the child",
+    "answer": "1"
+}, {
+    "question": "How did Sarai treat Hagar after she was with child (Gen. 16)?",
+    "option1": "She loved Hagar as a sister",
+    "option2": "She would not speak to Hagar",
+    "option3": "She despised Abram",
+    "option4": "She mistreated Hagar",
+    "answer": "4"
+}, {
+    "question": "Where did the angel of the Lord find Hagar (Gen. 16)?",
+    "option1": "Near a tree in the mountains",
+    "option2": "Near a spring in the desert",
+    "option3": "Near a river in the desert",
+    "option4": "Near the road to Egypt",
+    "answer": "2"
+}, {
+    "question": "The angel of the Lord told Hagar to _____ when she initially fled from Sarai (Gen. 16)",
+    "option1": "Return and give Abram a son",
+    "option2": "Stay in the desert until the child was born",
+    "option3": "Return and submit to Sarai",
+    "option4": "Go to Lot's house",
+    "answer": "3"
+}, {
+    "question": "The angel told Hagar to name her child ______ because the Lord had heard her (Gen. 16)",
+    "option1": "Ishmael",
+    "option2": "Naphtali",
+    "option3": "Abimelech",
+    "option4": "Isaac",
+    "answer": "1"
+}, {
+    "question": "Hagar's son was likened to what animal by the angel (Gen. 16)?",
+    "option1": "A horse",
+    "option2": "A donkey",
+    "option3": "A bird",
+    "option4": "A lion",
+    "answer": "2"
+}, {
+    "question": "What attitude would characterize how Hagar's son would relate to people (Gen. 16)?",
+    "option1": "Love",
+    "option2": "Justice",
+    "option3": "Fear",
+    "option4": "Hostility",
+    "answer": "4"
+}, {
+    "question": "How old was Abram when Hagar gave birth to their son (Gen. 16?",
+    "option1": "75",
+    "option2": "86",
+    "option3": "104",
+    "option4": "98",
+    "answer": "2"
+}, {
+    "question": "Each new born male in Abraham's household was to be circumcised on what day (Gen. 17)?",
+    "option1": "First",
+    "option2": "Seventh",
+    "option3": "Eighth",
+    "option4": "Fortieth",
+    "answer": "3"
+}, {
+    "question": "Sarai's name was changed to Sarah symbolizing she would be the  ________ (Gen. 17)",
+    "option1": "The mother of nations",
+    "option2": "The matriarch of Israel",
+    "option3": "The mother of a multitude",
+    "option4": "The blessed one",
+    "answer": "1"
+}, {
+    "question": "God said Abraham and Sarah's child would be named ________(Gen. 17)",
+    "option1": "Ishmael",
+    "option2": "Moriah",
+    "option3": "Isaac",
+    "option4": "Jacob",
+    "answer": "3"
+}
+];
